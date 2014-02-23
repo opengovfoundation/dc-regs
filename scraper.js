2680f16814d1037bc9150d737a18faddc8f4859e
Keith Porcaro <keithporcaro@Keiths-MacBook-Air.local>
2014-02-23 11:13:29 -0500

    minor scraper update

--- scraper.js
+++ scraper.js
@@ -1,8 +1,7 @@
 /*This scraper has been designed to use CasperJS/PhantomJS to scrape the DC municipal regulations.
 With a little tweaking, it'll happily burn through any page with large document sets. 
 
-The folder organization is an attempt to avoid problems with filename length limits. On OS X (untested elsewhere),
-the file names will span multiple lines, but should still all be there.
+The folder organization is an attempt to avoid problems with filename length limits. 
 
 TO-DO: -note regulations without accompanying document.
 -Incorporate renamer.rb bandages here (or incorporate filename-safe whitelist)
