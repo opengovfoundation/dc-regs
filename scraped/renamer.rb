20a80b8c1df908c916cbbd6b160ad06d01b46079
Keith Porcaro <keithporcaro@Keiths-MacBook-Air.local>
2014-02-23 10:25:11 -0500

    Fixed newlines in folder name, added new scrapes

--- scraped/renamer.rb
+++ scraped/renamer.rb
@@ -1,5 +1,7 @@
-filenames = Dir.glob("**/*")
+#This needs to be run more than once, because it changes folder names and everything gets upset. Just run it until it doesn't spit out errors.
 
-filenames.each do |filename|
+Dir.glob("**/*").each do |filename|	
 	File.rename(filename, filename.sub(/ \( Rename History \)/,''))
+	File.rename(filename, filename.sub(/\n/,' '))
+	File.rename(filename, filename.sub(/\*/,''))
 end
